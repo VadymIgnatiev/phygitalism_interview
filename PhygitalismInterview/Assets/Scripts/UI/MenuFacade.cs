ee85de887afcc44c12b2756c5bf0e250b208fc8b
Vadym Ignatiev <VadimIgna@yandex.ru>
2019-04-16 02:24:56 +0300

    added menu facade

---
+++ PhygitalismInterview/Assets/Scripts/UI/MenuFacade.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    public class MenuFacade : MonoBehaviour
+    {
+        public void Update()
+        {
+            
+        }
+    }
+}
